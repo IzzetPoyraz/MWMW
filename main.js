19551c037e1da8e6552b3ecd09c04636ad0a8c89
Izzet Poyraz <izzet9719@gmail.com>
2022-03-30 23:06:53 +0200

    POPUP

--- main.js
+++ main.js
@@ -17,3 +17,14 @@
         icon.setAttribute("aria-hidden", icon.getAttribute("aria-hidden") === "false" ? "true" : "false")
     }) 
 })();
+
+const open = document.getElementById('open');
+const container_popup = document.getElementById('container-popup');
+const sluiten = document.getElementById('sluiten');
+
+open.addEventListener('click', ()=>{
+    container_popup.classList.add('show');
+});
+sluiten.addEventListener('click', ()=>{
+    container_popup.classList.remove('show');
+});
